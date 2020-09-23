a072d8635b0ef8bf8933146dd03ff2e1789f0ace
MasterGowen <mastergowen@gmail.com>
2020-09-24 00:44:41 +0500

    DRlist paginate

--- lrr/repository/views.py
+++ lrr/repository/views.py
@@ -164,6 +164,7 @@ class ResultEduUpdateView(generic.UpdateView):
 
 
 class DigitalResourceListView(generic.ListView):
+    paginate_by = 12
     model = models.DigitalResource
     form_class = forms.DigitalResourceForm
 
@@ -425,11 +426,43 @@ def WorkPlanView(request):
                   {'academic_group': academic_group, 'obj_plan': obj_plan, 'person': person, 'status': status})
 
 
-def ResourceListView(request):
-    person = get_object_or_404(Person, user=request.user)
-    my_resource = models.DigitalResource.objects.filter(owner=person)
-    # status = models.DRStatus.objects.filter(digital_resource=for i in
-    return render(request, 'pages/resource_list.html', {'my_resource': my_resource})
+from django.http import Http404
+from django.utils.translation import gettext as _
+
+
+# class ResourceListView(generic.ListView):
+#     paginate_by = 12
+#     model = models.DigitalResource
+#     template_name = 'pages/resource_list.html'
+#
+#     def get(self, request, *args, **kwargs):
+#         self.object_list = self.get_queryset()
+#         allow_empty = self.get_allow_empty()
+#         # person = get_object_or_404(Person, user=request.user)
+#         # my_resource = models.DigitalResource.objects.filter(owner=person)
+#         if not allow_empty:
+#             # When pagination is enabled and object_list is a queryset,
+#             # it's better to do a cheap query than to load the unpaginated
+#             # queryset in memory.
+#             if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+#                 is_empty = not self.object_list.exists()
+#             else:
+#                 is_empty = not self.object_list
+#             if is_empty:
+#                 raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
+#                     'class_name': self.__class__.__name__,
+#                 })
+#         context = self.get_context_data()
+#         # context['my_resource']: my_resource
+#         return self.render_to_response(context)
+
+
+#
+# def ResourceListView(request):
+#     person = get_object_or_404(Person, user=request.user)
+#     my_resource = models.DigitalResource.objects.filter(owner=person)
+#     # status = models.DRStatus.objects.filter(digital_resource=for i in
+#     return render(request, 'pages/resource_list.html', {'my_resource': my_resource})
 
 
 def ExpertiseListView(request):
