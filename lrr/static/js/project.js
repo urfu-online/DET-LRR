59730ada1074d83bdca60c6e9201f7513e80d1f2
lea_alex <lea_alex@mail.ru>
2020-10-05 15:04:57 +0500

    Скрыть показать

--- lrr/static/js/project.js
+++ lrr/static/js/project.js
@@ -1,1 +1,6 @@
 /* Project specific Javascript goes here. */
+
+document.getElementById('resurse-list-hide').addEventListener("click", function(){
+    document.getElementById('resurse-list-hide').parentNode.classList.toggle('resurse-list-hide')
+
+})
