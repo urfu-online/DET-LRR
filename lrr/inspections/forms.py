3d61f3e4d21466cafcdab5abc8d1877c9b85d2ca
RaccoonBoyG <alexofficialkek@gmail.com>
2021-01-22 12:02:40 +0500

    form-control

--- lrr/inspections/forms.py
+++ lrr/inspections/forms.py
@@ -1,25 +1,83 @@
 from django import forms
 
 from lrr.inspections import models as inspections_models
+from lrr.repository import models as repository_models
+from lrr.complexes import models as complexes_models
+from lrr.users.models import Expert
 
 
 class ExpertiseForm(forms.ModelForm):
     class Meta:
         model = inspections_models.Expertise
-        fields = [
-            "digital_resource",
-            "date",
-            "subjects",
-            "directions",
-            "digital_complexes",
-            "expert",
-            "date_end",
-            "file",
-            "remarks",
-            "status",
-        ]
+        fields = '__all__'
+        # fields = [
+        #     "digital_resource",
+        #     "date",
+        #     "subjects",
+        #     "directions",
+        #     "digital_complexes",
+        #     "expert",
+        #     "date_end",
+        #     "file",
+        #     "remarks",
+        #     "status",np
+        # ]
         widgets = {
+            # 'digital_resource': forms.MultipleChoiceField(
+            #     attrs={
+            #         'class': 'form-control',
+            #         'required': 'false'
+            #     }
+            # ),
+            'date': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'required': 'true'
+                }
+            ),
             'file': forms.FileInput(
+                attrs={
+                    'class': 'form-control-file',
+                    'required': 'false'
+                }
+            ),
+            'subjects': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control',
+                    'required': 'true'
+                }
+            ),
+            'directions': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control-file',
+                    'required': 'false'
+                }
+            ),
+            'digital_complexes': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control',
+                    'required': 'true'
+                }
+            ),
+            'expert': forms.Select(
+                attrs={
+                    'class': 'form-control-file',
+                    'required': 'false'
+                }
+            ),
+            'date_end': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'required': 'true'
+                }
+            ),
+            'remarks': forms.Textarea(
+                attrs={
+                    'class': 'form-control-file',
+                    'required': 'false'
+                }
+            ),
+            'status': forms.Select(
                 attrs={
                     'class': 'form-control-file',
                     'required': 'false'
