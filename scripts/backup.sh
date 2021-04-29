646b644623d6b078059ad41d7954d898cd63e67d
MasterGowen <mastergowen@gmail.com>
2021-04-29 16:05:53 +0500

    fix bck script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,8 +2,5 @@
 
 profile="production"
 
-docker-compose -f ${profile}.yml down && \
-  docker-compose -f local.yml up -d postgres && \
-  docker-compose -f local.yml exec postgres backup
-docker cp `docker ps -aqf "name=lrr_postgres"`:/backups . && \
-  docker-compose -f local.yml down;
+docker-compose -f local.yml exec postgres backup
+docker cp `docker ps -aqf "name=lrr_postgres"`:/backups .
