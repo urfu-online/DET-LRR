c5e1b77d92df7ede29f3b4cb323028a87021152d
MasterGowen <mastergowen@gmail.com>
2021-10-22 08:32:24 +0500

    fix h2

--- lrr/static/js/project.js
+++ lrr/static/js/project.js
@@ -5,4 +5,40 @@ if (resourceListHideElement != null) {
     })
 }
 
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+
+        function alertize(alertEl) {
+            setTimeout(function () {
+                let pb = alertEl.querySelector(".progress-bar");
+                let progress = parseInt(pb.getAttribute('aria-valuenow'));
+                progress -= 5;
+                if (progress >= 0) {
+                    pb.setAttribute('aria-valuenow', progress)
+                    pb.style.width = progress + '%';
+                    alertize(alertEl);
+                } else {
+                    alertEl.remove();
+                    nextAlert();
+                }
+            }, 200);
+        }
+
+        function nextAlert() {
+            let alertsSidebar = document.querySelector('#alerts-sidebar');
+            if (!alertsSidebar) return;
+            let alerts = alertsSidebar.querySelectorAll(' .alert');
+            if (alerts.length > 0) {
+                alertize(alerts[alerts.length - 1]);
+
+            }
+        }
+
+        nextAlert();
+    }
+}
+
+
+
+
 
