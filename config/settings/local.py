1024bc3fe571fbd3f877674cf5e57fe3dd82fc76
MasterGowen <mastergowen@gmail.com>
2023-03-16 15:17:30 +0500

    add django-diagram

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,41 +9,41 @@ from .base import env
 # ------------------------------------------------------------------------------
 DEBUG = True
 SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="wqIxUuiCZKNIYaAxrc9NN7jLBlObdIgKSrFzjHI4ulxWiAgt52lcD4UAO2LmdbJY",
+    'DJANGO_SECRET_KEY',
+    default='wqIxUuiCZKNIYaAxrc9NN7jLBlObdIgKSrFzjHI4ulxWiAgt52lcD4UAO2LmdbJY',
 )
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "lrr.urfu.online", "192.168.0.151", "212.220.200.128"]
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'lrr.urfu.online', '192.168.0.151', '212.220.200.128']
 
 # CACHES
 # ------------------------------------------------------------------------------
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': env('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
 
-SELECT2_CACHE_BACKEND = "default"
+SELECT2_CACHE_BACKEND = 'default'
 
 # EMAIL
 # ------------------------------------------------------------------------------
-EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_HOST = env('EMAIL_HOST', default='mailhog')
 EMAIL_PORT = 1025
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
-INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
+INSTALLED_APPS = ['whitenoise.runserver_nostatic'] + INSTALLED_APPS  # noqa F405
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+INSTALLED_APPS += ['debug_toolbar', 'django_diagram']  # noqa F405
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
 DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
+    'DISABLE_PANELS': ['debug_toolbar.panels.redirects.RedirectsPanel'],
+    'SHOW_TEMPLATE_CONTEXT': True,
 }
 
 DEBUG_TOOLBAR_PANELS = [
@@ -54,22 +54,22 @@ DEBUG_TOOLBAR_PANELS = [
     # 'debug_toolbar.panels.headers.HeadersPanel',
     'debug_toolbar.panels.request.RequestPanel',
     'debug_toolbar.panels.sql.SQLPanel',
-    # 'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
     'debug_toolbar.panels.templates.TemplatesPanel',
     'debug_toolbar.panels.cache.CachePanel',
-    # 'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
     'debug_toolbar.panels.logging.LoggingPanel',
     # 'debug_toolbar.panels.redirects.RedirectsPanel',
     'debug_toolbar.panels.profiling.ProfilingPanel',
     # 'djdt_flamegraph.FlamegraphPanel',
 ]
 
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "192.168.0.151"]
-if env("USE_DOCKER") == "yes":
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', '192.168.0.151']
+if env('USE_DOCKER') == 'yes':
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+    INTERNAL_IPS += ['.'.join(ip.split('.')[:-1] + ['1']) for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
